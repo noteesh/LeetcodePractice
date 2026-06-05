ee34b1c04eb74d4cd20349d7d1dcd65d13e98e15
Nitesh Kanamarlapudi <154404228+noteesh@users.noreply.github.com>
2026-06-05 13:41:15 -0400

    Time: 7 ms (51.91%) | Memory: 24.9 MB (12.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hm = {}
+        '''
+            [1, 1, 1, 2, 2, 3]
+
+            [[], [3], [2], [1], [] ,[] ,[]]
+        '''
         l = [[] for i in range(len(nums) + 1)]
+        hs = {}
 
         for n in nums:
-            if n in hm:
-                hm[n] += 1
+            if n in hs:
+                hs[n] += 1
             else:
-                hm[n] = 1
+                hs[n] = 1
         
-        for key, v in hm.items():
-            l[v].append(key)
+        for n, c in hs.items():
+            l[c].append(n)
         
         ret = []
         for i in range(len(l) - 1, 0, -1):
@@ -18,4 +23,5 @@ class Solution:
                 ret.append(n)
                 if len(ret) == k:
                     return ret
-        return ret
+        return -1
+
