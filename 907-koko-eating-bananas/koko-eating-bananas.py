84cd998266c8454236da82b82474b5ebeb04230f
Nitesh Kanamarlapudi <154404228+noteesh@users.noreply.github.com>
2026-06-06 15:32:09 -0400

    Time: 170 ms (52.51%) | Memory: 20.7 MB (42.82%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
+
+        if h == len(piles):
+            return piles[-1]
+        elif h < len(piles):
+            return -1
+
+        k = 0
+        left = 1
+        right = piles[-1]
+        mid = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            curHours = self.eatingSpeed(piles, mid)
+
+            if curHours <= h:
+                k = mid
+                right = mid - 1
+                print(100+left)
+            elif curHours > h:
+                left = mid + 1
+        return k
+    
+    def eatingSpeed(self, piles: List[int], k: int) -> int:
+        h = 0
+        for n in piles:
+            h += (-(n // -k))
+        print(h)
+        return h
