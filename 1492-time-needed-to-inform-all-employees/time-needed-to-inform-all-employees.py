3c1bb66d9166a457dc818389e04de4b0e8a8c30d
Nitesh Kanamarlapudi <154404228+noteesh@users.noreply.github.com>
2026-06-16 10:39:13 -0400

    Time: 385 ms (5.37%) | Memory: 94.2 MB (13.08%) - LeetSync

--- 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.py
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.py
@@ -1,24 +1,12 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        if n == 1:
-            return 0
-        elif n == 2:
-            return 1
         hierarchy = defaultdict(list)
-
         for i, n in enumerate(manager):
             hierarchy[n].append(i)
-
-        st = []
-        count = 0
-        st.append((headID, 0))
-        maxCount = 0
-
-        while st:
-            temp, count = st.pop()
-            maxCount = max(maxCount, count)
-
-            for n in hierarchy[temp]:
-                st.append((n, count + informTime[temp]))
         
-        return maxCount
+        def dfs(node, time):
+            if not hierarchy[node]:
+                return time
+            return max(dfs(n, time + informTime[node]) for n in hierarchy[node])
+        
+        return dfs(headID, 0)
